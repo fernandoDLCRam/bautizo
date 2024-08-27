05848875c69f26bed00204c90f1a4836e54bf472
FernandoDLCRamirez <fdlcr1157@gmail.com>
2024-08-27 01:29:02 -0700

    add url

--- src/api/url.jsx
+++ src/api/url.jsx
@@ -2,5 +2,7 @@ import axios from "axios";
 
 export const RutaApi = axios.create({
   //NOTE: Aqui va la URL de la API, cambiar el string por la URL a utilizar
-  baseURL: "http://localhost:4000/",
+  //baseURL: "http://localhost:4000/",
+  baseURL: "https://ramonaapi.onrender.com/",
+  
 });
