c72ecd47be55cb8e9b8c5564fe8ab28cefe35e90
FernandoDLCRamirez <fdlcr1157@gmail.com>
2024-08-27 18:37:28 -0700

    fix value

--- src/api/url.jsx
+++ src/api/url.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
-require("dotenv").config();
 
+
+const KEY = import.meta.env.VITE_API_KEY;
 export const RutaApi = axios.create({
   //NOTE: Aqui va la URL de la API, cambiar el string por la URL a utilizar
   //baseURL: "http://localhost:4000/",
   baseURL: "https://ramonaapi.onrender.com/",
-  headers: {"Authorization": `Bearer ${process.env.VITE_API_KEY}`},
+  headers: {"Authorization": `Bearer ${KEY}`},
   
 });
