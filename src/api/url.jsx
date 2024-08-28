7f64b4215dc9539168c7242083fc023760305611
FernandoDLCRamirez <fdlcr1157@gmail.com>
2024-08-27 18:04:24 -0700

    add reference to API

--- src/api/url.jsx
+++ src/api/url.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
+require("dotenv").config();
 
 export const RutaApi = axios.create({
   //NOTE: Aqui va la URL de la API, cambiar el string por la URL a utilizar
   //baseURL: "http://localhost:4000/",
   baseURL: "https://ramonaapi.onrender.com/",
+  headers: {"Authorization": `Bearer ${process.env.API_KEY}`},
   
 });
