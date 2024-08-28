a8e615fd677fdcc8c34cf05b7e71ea91d784268b
FernandoDLCRamirez <fdlcr1157@gmail.com>
2024-08-27 18:57:35 -0700

    add bautizo route

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -7,10 +7,9 @@ const Router = () => {
   return (
     <Routes>
       <Route>
-        <Route path="/" element={<Landing />} />
+        <Route path="/bautizo" element={<Landing />} />
         <Route path="/itinerario" element={<Itinerario />} />
-        <Route path="*" element={<Navigate to={<Landing />} />} />
-        <Route path="/404" element={<Navigate to={<Landing />} />} />
+        <Route path="*" element={<Navigate to={"/bautizo"} />} />
       </Route>
     </Routes>
   );
