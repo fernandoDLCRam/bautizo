2512ce4d041d141561ea9a3ee4ab61c6747dab2b
FernandoDLCRamirez <fdlcr1157@gmail.com>
2024-08-27 18:18:08 -0700

    commit

--- src/api/url.jsx
+++ src/api/url.jsx
@@ -5,6 +5,6 @@ export const RutaApi = axios.create({
   //NOTE: Aqui va la URL de la API, cambiar el string por la URL a utilizar
   //baseURL: "http://localhost:4000/",
   baseURL: "https://ramonaapi.onrender.com/",
-  headers: {"Authorization": `Bearer ${process.env.API_KEY}`},
+  headers: {"Authorization": `Bearer ${process.env.VITE_API_KEY}`},
   
 });
