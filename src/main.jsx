e8c4efe07f05792acc49ad191d55cea81ae430c0
FernandoDLCRamirez <fdlcr1157@gmail.com>
2024-08-27 20:07:19 -0700

    change to hashroute

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/index.css";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+
+import Landing from "../components/Landing";
+import Itinerario from "../components/Itinerario";
+
+const router = createHashRouter([
+  {
+    path: "/bautizo",
+    element: <Landing />,
+  },
+  {
+    path: "/bautizo/itinerario",
+    element: <Itinerario />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <RouterProvider router={router}>
       <App />
-    </BrowserRouter>
+    </RouterProvider>
   </React.StrictMode>
 );
